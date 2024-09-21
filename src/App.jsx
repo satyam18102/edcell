0bb9a6e88ac3d8861c685768d5b7232fbb9c5c87
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-09-21 18:23:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
             }
           />
                <Route
-                path="*
+                path="*"
                 element={
                   <>
                     <Navbar />
