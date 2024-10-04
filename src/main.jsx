90d420d9b783d1a5a70b33ba266a50a2ca5e0591
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-10-04 16:58:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import {Analytics} from "@vercel/analytics/react";
+import {inject} from "@vercel/analytics/react";
+inject();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Analytics/>
     <App/>
   </React.StrictMode>
 );
