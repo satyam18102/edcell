c8a7e40a333de644c807ecb3fae74ca8778c042f
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-10-28 15:33:08 +0530

    Update FAQ.jsx

--- src/components/sparkupsummit/FAQ.jsx
+++ src/components/sparkupsummit/FAQ.jsx
@@ -5,16 +5,24 @@ export default function FAQ() {
         <div className="accordion-container">
             <h1>Frequently asked Questions</h1>
             <Accordion
-                title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos veniam totam ad molestiae dicta deleniti eos autem nemo dolore modi temporibus voluptas, recusandae impedit beatae vero aut molestias, laudantium id."
+                title="When and where will the summit be held?"
+                content="Sparkup Summit will be held from 29th November to 1st December at Silicon University, Bhubaneswar, Odisha."
             />
             <Accordion
-                title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos veniam totam ad molestiae dicta deleniti eos autem nemo dolore modi temporibus voluptas, recusandae impedit beatae vero aut molestias, laudantium id."
+                title="Who can attend the Sparkup Summit?"
+                content="The Sparkup Summit is open to school and college students from different regions, as well as budding startups, investors, and entrepreneurs."
+            />
+             <Accordion
+                title="How can I register for the summit?"
+                content="Registration details and fees will be available on the official Sparkup Summit website."
             />
             <Accordion
-                title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos veniam totam ad molestiae dicta deleniti eos autem nemo dolore modi temporibus voluptas, recusandae impedit beatae vero aut molestias, laudantium id."
+                title="What can attendees expect at the summit?"
+                content="Attendees can expect inspiring keynote speeches from industry leaders, interactive workshops, panel discussions, networking opportunities, and a vibrant entrepreneurial environment."
+            />
+             <Accordion
+                title="Can I participate in the summit virtually?"
+                content="Yes, you can be a part of Sparkup Summit virtually by participating in our online events."
             />
         </div>
     )
