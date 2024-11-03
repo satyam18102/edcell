fdd87b1484e9bf723a54981d152a90f162cdb81b
Lavkush Solanki <156509767+LavkushSolanki@users.noreply.github.com>
2024-11-03 11:33:26 +0530

    Update Footer.jsx added livecount name

--- src/components/sparkupsummit/Footer.jsx
+++ src/components/sparkupsummit/Footer.jsx
@@ -9,7 +9,7 @@ import {
 
 const Footer = () => {
   return (
-    <div id="contact" >
+    <div id="contact">
       <div className="bg-[#100a2e] z-40 text-white py-4 sm:py-6 px-4 sm:px-8 lg:px-12 footer bg-[#7b070be7] bg-blend-darken bg-cover">
         <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center">
           {/* Left Section - Logo and Address */}
@@ -50,6 +50,17 @@ const Footer = () => {
                 <FaInstagram />
               </a>
             </div>
+            <br />
+            <div className="text-center flex justify-center align-middle mt-3">
+              Page Visits: &nbsp;
+              <img
+                className="w-28 rounded-xl"
+                src="https://hitwebcounter.com/counter/counter.php?page=17191340&style=0009&nbdigits=5&type=page&initCount=100"
+                title="Counter Widget"
+                Alt="Visit counter For Websites"
+                border="0"
+              />
+            </div>
           </div>
 
           {/* Right Section - Copyright and Additional Info */}
@@ -62,6 +73,47 @@ const Footer = () => {
               Email: edcell@silicon.ac.in
             </p>
           </div>
+        </div>
+        <br />
+        <div
+          style={{ fontFamily: "serif" }}
+          className="text-center font-bold text-[#f6c572]"
+        >
+          Coded under the mask by <br />
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/rahul-kumar-singh-367b14256"
+          >
+            <span className="text-white underline">Rahul</span>{" "}
+          </a>
+          &nbsp;&nbsp;
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/piyush-kumar-90b2a02aa?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          >
+            <span className="text-white underline"> Piyush</span>{" "}
+          </a>
+          &nbsp;&nbsp;
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/ayushmanshaw?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          >
+            <span className="text-white underline"> Ayushman</span>{" "}
+          </a>
+          &nbsp;&nbsp;
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/lavkush-solanki-3577a4254"
+          >
+            <span className="text-white underline"> Lavkush</span>{" "}
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/satyam-sinha-a12748268"
+          >
+            <span className="text-white underline">Satyam</span>
+          </a>
         </div>
       </div>
     </div>
