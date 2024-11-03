c4841ff3e518a8080fd2a2cdc382a85793054d38
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-03 23:39:51 +0530

    Update Home_Center.jsx

--- src/components/sparkupsummit/Home_Center.jsx
+++ src/components/sparkupsummit/Home_Center.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import gunImage from "/sparkupsummit/images/home_center/gun2.png";
-import gun1Image from "/sparkupsummit/images/home_center/gun1.png";
-import maskImage from "/sparkupsummit/images/home_center/mask2.jpg";
+import gunImage from "/sparkupsummit/assets/home_center/shot2.png";
+import gun1Image from "/sparkupsummit/assets/home_center/shot1.png";
+import maskImage from "/sparkupsummit/assets/home_center/mask4.png";
 
 const Card = () => {
   const [rotation, setRotation] = useState(0);
@@ -21,35 +21,35 @@ const Card = () => {
   }, []);
 
   return (
-    <div className="bg-black max-h-screen  w-full flex flex-col justify-end items-center text-white p-2">
+    <div className="bg-black max-h-screen  w-full flex flex-col justify-start items-center text-white p-2">
       <img
         src={maskImage}
         alt="Mask"
-        className="w-64 h-auto mt-[70px] md:mt-14 sm:w-80 sm:h-96 md:w-96 md:h-[500px] lg:h-[450px]  xl:h-[300px] object-contain mb-16"
+        className="w-64 h-auto mt-[80px] md:mt-14   md:w-96 md:h-[500px] lg:h-[450px]  xl:h-[370px] object-contain mb-8"
       />
 
       {/* Container for Rotating Images and Text */}
-      <div className="flex flex-row items-center justify-center space-x-[-36px] mb-4 md:mb-0 mt-[-50px]  lg:space-x-[-50px]">
+      <div className="flex flex-row items-center justify-center space-x-[-86px]  md:mb-[-30px] mt-[-60px]  lg:space-x-[-130px]">
         {/* First Image Rotates in Opposite Direction, Starting at 45 Degrees */}
         <img
           src={gun1Image}
           alt="Gun1"
-          className="w-32 h-auto sm:w-64 md:w-80 lg:w-96 xl:w-[160px] object-contain" // Increased sizes
-          style={{ transform: `rotate(${40 - rotation}deg)` }} degrees
+          className="w-48 h-auto sm:w-64 md:w-80 lg:w-96 xl:w-[280px] object-contain" // Increased sizes
+          style={{ transform: `rotate(${5-rotation}deg)` }}
         />
 
         {/* Second Image Rotates in the Same Direction, Starting at 45 Degrees */}
         <img
           src={gunImage}
           alt="Gun"
-          className="w-32 h-auto ml-[-20px] md:w-80 lg:w-96 xl:w-[160px] object-contain" // Increased sizes
-          style={{ transform: `rotate(${rotation - 40}deg)` }} // Rotating in the same direction from 45 degrees
+          className="w-48 h-auto ml-[-20px] md:w-80 lg:w-96 xl:w-[280px] object-contain" // Increased sizes
+           style={{ transform: `rotate(${rotation-5}deg)` }} // Rotating in the same direction from 45 degrees
         />
       </div>
 
       {/* Text below the images */}
       <div
-        className="text-center font-anton text-4xl lg:text-5xl xl:text-7xl text-white my-4"
+        className="text-center font-anton text-4xl lg:text-6xl text-white my-4"
       >
         SPARK UP SUMMIT
       </div>
