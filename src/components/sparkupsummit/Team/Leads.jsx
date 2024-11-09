002fe1faa987a777103f2bf8f33790429c38e382
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-09 19:52:10 +0530

    Update Leads.jsx

--- src/components/sparkupsummit/Team/Leads.jsx
+++ src/components/sparkupsummit/Team/Leads.jsx
@@ -42,7 +42,7 @@ const members = [
     name: "Gomesh Aggrawal",
     year: "Graphics",
     img: "/sparkupsummit/images/members/m7.jpeg",
-    img2: "/sparkupsummit/images/members/gomesh.jpg",
+    img2: "/team/gomesh.jpg",
   },
   {
     name: "Anmol Singh",
