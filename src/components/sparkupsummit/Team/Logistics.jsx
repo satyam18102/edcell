0a1124d4c720e5bd324ca6424f943299b422dbed
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-09 19:58:22 +0530

    Update Logistics.jsx

--- src/components/sparkupsummit/Team/Logistics.jsx
+++ src/components/sparkupsummit/Team/Logistics.jsx
@@ -5,18 +5,19 @@ const members = [
   {
     name: "Anmol Singh",
     year: "3rd year",
-    img: "assets/images/members/m7.jpeg",
-    img2: "assets/images/members/anmol.JPG",
+    img: "/sparkupsummit/images/members/m7.jpeg",
+    img2: "/sparkupsummit/images/members/anmol.JPG",
   },
   {
     name: "Ayushman Pani",
     year: "3rd year",
-    img: "assets/images/members/m7.jpeg",
-    img2: "assets/images/members/icon.png",
+    img: "/sparkupsummit/images/members/m7.jpeg",
+    img2: "/sparkupsummit/images/members/icon.png",
   },
 ];
 
 const Logistics = () => {
+  window.scroll(0,0)
   return (
     <div className="bg-black font-anton overflow-x-hidden">
       <h1
