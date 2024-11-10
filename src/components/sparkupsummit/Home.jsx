179c9c4c57aa0360689e47942c7d0a2aa3bf45dc
Lavkush Solanki <156509767+LavkushSolanki@users.noreply.github.com>
2024-11-10 23:51:53 +0530

    Update Home.jsx by lavkush final heyy

--- src/components/sparkupsummit/Home.jsx
+++ src/components/sparkupsummit/Home.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import './Home.css'; // Make sure to update your CSS
+import React from "react";
 import ParticlesComponent from "./Particles"; // Import the Particles component
-import Home_Center from "./Home_Center"
+import Home_Center from "./Home_center";
+import "./Home.css"; // Make sure to update your CSS
 
 const Home = () => {
   return (
-    <div className="home-container " id='home'>
-      <div className={(window.innerWidth < 400)?"flex-none":"first-container"}>
-        <ParticlesComponent id="particles" />
+    <div className="bg-gradient-to-r from-red-500 to-black">
+      <div id="home" className="home-container">
+        <div
+          className={window.innerWidth < 400 ? "flex-none" : "first-container"}
+        >
+          <ParticlesComponent id="particles" />
+        </div>
+        <div className="h-screen md:w-1/3 w-screen pt-6 responsive-bg">
+          <Home_Center />
+        </div>
+        <div
+          className={window.innerWidth < 400 ? "flex-none" : "first-container"}
+        >
+          <ParticlesComponent id="particles1" />
+        </div>
       </div>
-      <div className='h-screen md:w-1/3 w-screen pt-10 md:pt-6'>
-        <Home_Center/>
-      </div>
-      <div className={(window.innerWidth < 400)?"flex-none":"first-container"}>
-        <ParticlesComponent id="particles1" />
-      </div>
-      {/* <div className='md:h-screen md:w-1/3 w-0 h-0'>
-        <img src='/assets/images/backgrounds/home_bg1.jpg' className='h-screen'/>
-      </div>   */}
     </div>
   );
 };
