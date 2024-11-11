43049c2967f15d21df972b2c957536b17d24f236
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-11 17:07:20 +0530

    Update Vision.jsx

--- src/components/sparkupsummit/Vision.jsx
+++ src/components/sparkupsummit/Vision.jsx
@@ -34,7 +34,7 @@ export const Vision = () => {
             className="rounded-3xl w-auto h-[300px] lg:w-auto lg:h-[355px] transform-image"
             width="560"
             height="315"
-            src="/sparkupsummit/images/mascot.gif.gif"
+            src="/sparkupsummit/images/mascot.png"
             alt="Vision"
           />
         </div>
