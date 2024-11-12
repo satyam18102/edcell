a648b045c2b3ccdff35a184bc7fa1be87714675f
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-13 00:35:21 +0530

    Update Vision.jsx

--- src/components/sparkupsummit/Vision.jsx
+++ src/components/sparkupsummit/Vision.jsx
@@ -12,18 +12,16 @@ export const Vision = () => {
       <div className="flex flex-col-reverse md:flex-row bg-cover bg-center">
         <div className="md:w-1/2 sm:text-sm md:text-lg lg:text-lg text-sm font-light w-full flex items-center justify-center text-[#DEC7A0] p-8 md:p-12">
           <p className="leading-relaxed max-w-xl">
-            Spark Up Summit aims to spark an entrepreneurial spirit among diverse
-            participants by fostering a collaborative environment for idea
-            exchange, experience sharing, and mentorship, which are key elements
-            for success. Through networking sessions, various competitions, and
-            mentorship programs, young entrepreneurs gain vital skills and
-            connections to scale their ventures while emphasizing sustainable
-            practices and ethical decision-making. By embracing diversity and
-            promoting collaboration, E-fest empowers students to unleash their
-            potential and drive positive change in their communities, expanding
-            our reach through alliances with various stakeholders to create
-            opportunities for all to engage in and contribute to our vibrant
-            community
+            Spark Up Summit aims to spark an entrepreneurial spirit among diverse 
+            participants by fostering a collaborative environment for idea exchange, 
+            experience sharing, and mentorship, which are key elements for success.
+            Through networking sessions, various competitions, and mentorship programs, 
+            young entrepreneurs gain vital skills and connections to scale their ventures while 
+            emphasising sustainable practices and ethical decision-making. By embracing diversity 
+            and promoting collaboration, E-fest empowers students to unleash their potential 
+            and drive positive change in their communities, expanding our
+            reach through alliances with various stakeholders to create 
+            opportunities for all to engage in and contribute to our vibrant community.
           </p>
         </div>
         <div className="md:w-1/2 w-full flex items-center justify-center p-4">
