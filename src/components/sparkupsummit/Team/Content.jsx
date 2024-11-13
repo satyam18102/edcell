2b03cdc1270674cc04bb7c5617fa9bdf1e085b6b
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-13 16:47:46 +0530

    Update Content.jsx

--- src/components/sparkupsummit/Team/Content.jsx
+++ src/components/sparkupsummit/Team/Content.jsx
@@ -36,7 +36,7 @@ const members = [
     name: "Omkarnath Mohapatra",
     year: "2nd year",
     img: "/sparkupsummit/images/members/m7.jpeg",
-    img2: "/sparkupsummit/images/members/icon.png",
+    img2: "/sparkupsummit/images/members/omkarnath.jpg",
   },
    {
     name: "Jasdeep Singh",
