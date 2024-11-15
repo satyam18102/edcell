272ff04488cbb51c8b149c1abf1a42b69dab3e71
RahulKumarSingh123 <123647773+RahulKumarSingh123@users.noreply.github.com>
2024-11-15 15:05:23 +0530

    Update SocialMedia.jsx

--- src/components/sparkupsummit/Team/SocialMedia.jsx
+++ src/components/sparkupsummit/Team/SocialMedia.jsx
@@ -18,7 +18,7 @@ const members = [
     name: "Shubham Pani",
     year: "3rd year",
     img: "/sparkupsummit/images/members/m7.jpeg",
-    img2: "/sparkupsummit/images/members/icon.png",
+    img2: "/sparkupsummit/images/members/shubhampani.jpg",
   },
   {
     name: "Megha Patro",
