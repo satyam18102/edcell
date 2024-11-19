e99ed4f1aa3b05b387eb70282da3be7d8f7e8ec0
satyam18102 <eee.23beec02@silicon.ac.in>
2024-11-20 00:05:56 +0530

    Update Content.jsx

--- src/components/sparkupsummit/Team/Content.jsx
+++ src/components/sparkupsummit/Team/Content.jsx
@@ -33,7 +33,7 @@ const members = [
     img2: "/sparkupsummit/images/members/puja2.jpg",
   },
   {
-    name: "Omkarnath Mohapatra",
+    name: "Omkarnath",
     year: "2nd year",
     img: "/sparkupsummit/images/members/m7.jpeg",
     img2: "/sparkupsummit/images/members/omkarnath.jpg",
