b42602ed41808fd19c99aff563f24ab0c26e2769
satyam18102 <satyamsinha340@gmail.com>
2025-12-08 03:26:48 +0530

    Adding Event

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,6 +16,10 @@ function Cards() {
       id: "sparkupsummit",
       image: "/assets/images/events/EFEST6.png",
     },
+    {
+      id: "comingsoon",
+      image: "/assets/images/events/coming.png",
+    },
   ];
 
   return (
@@ -26,7 +30,7 @@ function Cards() {
         </h2>
       </div>
 
-      <div className="mt-10 mx-8">
+      <div className="mt-10 mx-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {eventsData.map((events) => (
             <div key={events.id} className="flex justify-center">
@@ -38,11 +42,11 @@ function Cards() {
                     alt="Event"
                   />
                   <div className="absolute bottom-4 w-full flex justify-center">
-                  <Link to={"/"+events.id}> 
+                  <Link to={(events.id=="comingsoon")?("/"):("/"+events.id)}> 
                     <button className="relative hover:scale-105 inline-flex md:h-12 md:w-42 sm:h-10 sm:wd-40 h-10 w-40  lg:h-16 lg:w-48 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2">
                       <span className="absolute inset-0 bg-gradient-to-r from-purple-500 to-indigo-500 rounded-full animate-pulse" />
                       <span className="relative inline-flex h-full w-full items-center justify-center rounded-full bg-gray-900 text-white text-xl font-semibold transition-transform duration-300 ">
-                        {(events.id=="sparkupsummit")?"Explore":"Explore"}
+                        {(events.id=="comingsoon")?"Coming Soon":"Explore"}
                       </span>
                     </button>
                   </Link>
